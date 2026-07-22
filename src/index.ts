9ef8d389eb65baf1458626a0db888272c74d7b0e
amucia-2025337 <amucia-2025337@kinal.edu.gt>
2026-07-22 11:53:00 -0600

    feat: Se adapto para poder ejecutar el servidor en segundo plano

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hola")
+import { iniciarServidor } from "./api/server.js";
+import { ejecutarPruebasBackend } from "./utils/testRunner.js";
+
+async function main() {
+    iniciarServidor();
+
+    await ejecutarPruebasBackend();
+}
+
+main();
