fc37e623f36c1259d6b074806563a2592199627e
andersonmucia-2025337 <andersonmucia-2025337@kinal.edu.gt>
2026-07-28 11:49:06 -0600

    feat: Se actualizo la configuracion por error en clases

--- src/data/db.ts
+++ src/data/db.ts
@@ -2,8 +2,8 @@ import * as mysql from "mysql2/promise";
 
 export const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "IN5CM",
-  password: process.env.DB_PASSWORD || "?donmoA5m@",
+  user: process.env.DB_USER || "IN4AM",
+  password: process.env.DB_PASSWORD || "Kinal@2025AM",
   database: process.env.DB_NAME || "dbgestiondecomedoressociales_in5cm",
   waitForConnections: true,
   connectionLimit: 10,
